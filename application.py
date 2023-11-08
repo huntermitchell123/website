c684885e41648987b45f4fe2a962d5988d0f69ef
Hunter Mitchell <hunter.f.mitchell@gmail.com>
2023-11-08 15:26:51 -0700

    trying custom decorator function CORS solution

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 import base64
+from datetime import timedelta
+from functools import update_wrapper
 import io
 
 import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, current_app
 from flask_cors import CORS
 import tflite_runtime.interpreter as tflite
 from PIL import Image
@@ -36,6 +38,64 @@ output_details_age = interpreter_age.get_output_details()
 output_details_gender = interpreter_gender.get_output_details()
 
 
+### CORS Decorator Function ###
+
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
+                attach_to_all=True, automatic_options=True):
+    """Decorator function that allows crossdomain requests.
+      Courtesy of
+      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
+    """
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    # use str instead of basestring if using Python 3.x
+    if headers is not None and not isinstance(headers, list):
+        headers = ', '.join(x.upper() for x in headers)
+    # use str instead of basestring if using Python 3.x
+    if not isinstance(origin, list):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        """ Determines which methods are allowed
+        """
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        """The decorator function
+        """
+        def wrapped_function(*args, **kwargs):
+            """Caries out the actual cross domain code
+            """
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
+
 ### DEFINE ROUTES ###
 
 @app.route('/')
@@ -48,7 +108,8 @@ def project_get():
     return render_template('websiteProject.html')
 
 
-@app.route('/project', methods=['POST'])
+@app.route('/project', methods=['POST', 'OPTIONS'])
+@crossdomain(origin='*')
 def project_post():
 
     image_b64 = request.values['imageBase64']
